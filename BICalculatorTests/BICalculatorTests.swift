64ea311e2a6fac70727cbdfbe625e2340852b285
Cognizant2 <cognizant2@cognizant2s-mac-mini.local>
2016-05-09 16:55:14 +0530

    Initial Commit

---
+++ BICalculatorTests/BICalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  BICalculatorTests.swift
+//  BICalculatorTests
+//
+//  Created by Cognizant2 on 09/05/16.
+//  Copyright © 2016 Cognizant2. All rights reserved.
+//
+
+import XCTest
+@testable import BICalculator
+
+class BICalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
